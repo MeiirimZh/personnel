372ad6f2c78ef5cce92eb92d1adb656efa58612c
Meiirim <Meiirim>
2024-09-23 14:01:35 +0500

    Imported psycopg2 and config parameters

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
+import psycopg2
 from PyQt5 import QtCore, QtGui, QtWidgets
 from mainwindow import Ui_MainWindow
+from config import host, username, password, db_name
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
